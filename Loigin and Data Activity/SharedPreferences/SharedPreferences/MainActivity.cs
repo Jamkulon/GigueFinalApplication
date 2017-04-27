ff4f93a655e9fb7abc446dd38ba99a466b8b25cb
joelrhardisty <joelrhardisty@gmail.com>
2017-04-27 11:51:23 -0700

    Added Registration button to login page and having it navigate to Musician or User selection and pointing them to seperate registration pages

--- Loigin and Data Activity/SharedPreferences/SharedPreferences/MainActivity.cs
+++ Loigin and Data Activity/SharedPreferences/SharedPreferences/MainActivity.cs
@@ -19,6 +19,7 @@ namespace SharedPreferences
 
         EditText mUsername;
 
+        Button mRegister;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -30,9 +31,16 @@ namespace SharedPreferences
             mRelativeLayout = FindViewById<RelativeLayout>(Resource.Id.mainView);
             mUsername = FindViewById<EditText>(Resource.Id.txtUserName);
             mButton = FindViewById<Button>(Resource.Id.btnLogin);
+            mRegister = FindViewById<Button>(Resource.Id.btnRegister);
             mButton.Click += mButton_Click;
             mRelativeLayout.Click += mRelativeLayout_Click;
+            mRegister.Click += mRegister_Click;
 
+        }
+        void mRegister_Click(object sender, EventArgs r)
+        {
+            Intent intent = new Intent(this, typeof(RegistrationActivity));
+            this.StartActivity(intent);
         }
 
         void mButton_Click(object sender, EventArgs e)
---
+++ Loigin and Data Activity/SharedPreferences/SharedPreferences/RegistrationActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SharedPreferences
+{
+    [Activity(Label = "RegistrationActivity")]
+    public class RegistrationActivity : Activity
+    {
+        Button mMusician;
+        Button mUser;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Registration);
+
+            mMusician = FindViewById<Button>(Resource.Id.btnMusician);
+            mUser = FindViewById<Button>(Resource.Id.btnUser);
+            mMusician.Click += mMusician_Click;
+            mUser.Click += mUser_Click;
+            // Create your application here
+        }
+
+        void mMusician_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(MusicianActivity));
+            this.StartActivity(intent);
+        }
+
+        void mUser_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(UserActivity));
+            this.StartActivity(intent);
+        }
+    }
+}
