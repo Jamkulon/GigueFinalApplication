e28e03a229bca040c4f48b57469eb777ef7fea16
Bob Townsend <bt.tests.it@gmail.com>
2017-05-05 14:56:54 -0700

    Updated AppUser to fix UserMusician

--- Gigue_Xamarin_Android/Gigue/DataObjects/AppUser.cs
+++ Gigue_Xamarin_Android/Gigue/DataObjects/AppUser.cs
@@ -14,7 +14,7 @@ namespace Gigue.DataObjects
         public string Email { get; set; }
         public bool? ReceiveAdvertisements { get; set; }
         public bool? IsMusician { get; set; }
-        public ICollection<UserMusician> Musicians { get; set; }
+        public ICollection<UserMusician> UserMusicians { get; set; }
         public ICollection<UserFavoriteMusician> UserFavoriteMusicians { get; set; }
         public ICollection<UserMusicianRating> UserMusicianRatings { get; set; }
     }
