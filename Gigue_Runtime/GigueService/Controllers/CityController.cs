e294bacf8ced88971d304e92ace502afe5422397
DESKTOP-8H5S41D\Geoffrey <geoffreyburley@gmail.com>
2017-05-16 18:51:33 -0700

    Added CityController, CityService that create the drop down list of cities.  Added StateController and StateService that create the drop down list of states.

---
+++ Gigue_Runtime/GigueService/Controllers/CityController.cs
@@ -0,0 +1,79 @@
+﻿using GigueService.Models;
+using GigueService.Services;
+using Microsoft.Azure.Mobile.Server.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigueService.Controllers
+{
+    // Use the MobileAppController attribute for each ApiController you want to use  
+    // from your mobile clients 
+
+    [MobileAppController]
+    public class CityController : ApiController
+    {
+        //=================================================================================
+        //Fields.
+        //=================================================================================
+        public CityService service;
+        public Repo repo;
+        public GigueContext db;
+        //=================================================================================
+        //Properties.
+        //=================================================================================
+        //=================================================================================
+        //Constructor().
+        //=================================================================================
+        public CityController()
+        {
+            db = new GigueContext();
+            repo = new Repo(db);
+            service = new CityService(repo, db);
+
+        }
+        //=================================================================================
+        //Methods()
+        //=================================================================================
+        [HttpGet]
+        // GET: api/City
+        //This method gets a list of unique city names that can be used in a drop down menu on the application.   
+        public List<string> Get()
+        {
+            try
+            {
+                List<string> cities = service.GetCities();
+                return cities;
+            }
+            catch
+            {
+                BadRequest("No data found to match request");
+            }
+            return null;
+        }
+        //===============================================================================================
+        // GET: api/City/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/City
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/City/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/City/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Gigue_Runtime/GigueService/Services/CityService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigueService.Controllers;
+using GigueService.Models;
+
+namespace GigueService.Services
+{
+    public class CityService
+    {
+        //=================================================================
+        //Fields.
+        //=================================================================
+        private Repo _repo;
+        private GigueContext _db;
+        //=================================================================
+        //Properties.
+        //=================================================================
+
+        //=================================================================
+        //Constructor().
+        //=================================================================
+        public CityService(Repo repo, GigueContext db)
+        {
+            _repo = repo;
+            _db = db;
+        }
+        //=================================================================
+        //Methods().
+        //=================================================================
+
+        public List<string> GetCities()
+        {
+            List<AppUser> aus = _repo.Query<AppUser>().ToList();
+            List<string> cities = new List<string>();
+            if (aus == null)
+            {
+                return null;
+            }
+            else
+            {
+                cities.Add(aus[0].City);
+                for (int i = 1; i < aus.Count; i++)
+                {
+                    bool addToCities = true;
+                    string testCity1 = aus[i].City;
+                    for (int j = 0; j < cities.Count; j++)
+                    {
+                        string testCity2 = cities[j];
+                        if (testCity1 == testCity2)
+                        {
+                            addToCities = false;
+                        }
+                        else { }
+                    }
+                    if (addToCities == true && testCity1 != "")
+                    {
+                        cities.Add(testCity1);
+                    }
+
+                }
+                return cities;
+            }
+        }
+    }
+}
